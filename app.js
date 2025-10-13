f076cee68c164828924aa08ff3de635af48f3e7d
Youssef Ahmed <youssefahmedb90@gmail.com>
2025-10-13 17:46:41 +0300

    feat: modify tourModel and create import script

--- app.js
+++ app.js
@@ -9,12 +9,4 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/tours', tourRouter);
 
-// Test route
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Hello from the server side!'
-  });
-});
-
 module.exports = app;
