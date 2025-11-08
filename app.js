1c498f5000c17625a182656c56bf6e722bbc5450
Youssef Ahmed <youssefahmedb90@gmail.com>
2025-11-08 13:29:02 +0200

    feat: error handling section implementation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRouter');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -10,5 +12,13 @@ app.use(express.json());
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
+
+//Error handling for undefined routes
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE_CONNECTION_STRING.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
@@ -14,11 +21,19 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true
   })
-  .then(() => console.log("DB connection successful!"));
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  Server.close(() => {
+    process.exit(1);
+  });
 });
